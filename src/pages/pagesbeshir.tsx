263f58fe805c3d2abfd8dae13532c9e42c5b33f7
beshir23 <beshirkareem2@gmail.com>
2023-11-06 14:54:57 +0100

    prova

--- src/pages/pagesbeshir.tsx
+++ src/pages/pagesbeshir.tsx
@@ -6,5 +6,6 @@ export default function pagesbeshir() {
   export default function pagesbeshir() {
     return (
       <h1>kareem beshir</h1>
+      <p> es 3 punto 6 ho usato git rebase ma non sono sicuro sia giusto</p>
     );
   }
