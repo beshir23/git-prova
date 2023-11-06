53e99b0b394c3c3e4d1c70ea26b9b41173a9f976
beshir23 <beshirkareem2@gmail.com>
2023-11-06 14:51:41 +0100

    prova

---
+++ src/components/beshir/logo.tsx
@@ -0,0 +1,5 @@
+export default function pagesbeshir() {
+    return (
+      <h1>kareem beshir</h1>
+    );
+  }
---
+++ src/pages/pagesbeshir.tsx
@@ -0,0 +1,10 @@
+export default function pagesbeshir() {
+    return (
+      <h1>kareem beshir</h1>
+    );
+  }
+  export default function pagesbeshir() {
+    return (
+      <h1>kareem beshir</h1>
+    );
+  }
