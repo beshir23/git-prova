b570f084d3d4f1a0a291ab93d13871aa4f6f0919
beshir23 <beshirkareem2@gmail.com>
2023-11-06 15:06:52 +0100

    merge

---
+++ src/pages/pagesbeshir.jsx
@@ -0,0 +1,11 @@
+export default function pagesbeshir() {
+    return (
+      <h1>kareem beshir</h1>
+    );
+  }
+  export default function pagesbeshir() {
+    return (
+      <h1>kareem beshir</h1>
+      <p> es 3 punto 6 ho usato git rebase ma non sono sicuro sia giusto</p>
+    );
+  }
---
+++ src/pages/pagesbeshir.tsx
@@ -0,0 +1,12 @@
+export default function pagesbeshir() {
+    return (
+      <h1>kareem beshir</h1>
+    );
+  }
+  export default function pagesbeshir() {
+    return (
+      <h1>kareem beshir</h1>
+      <p> es 3 punto 5/6 ho usato git rebase ma non sono sicuro sia giusto</p>
+      <p> es 4 punto 1/2 ho usato git merge</p>
+    );
+  }
