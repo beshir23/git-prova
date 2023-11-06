f32a58cf5b66b42a2da4986896f762c7770ee186
beshir23 <128391475+beshir23@users.noreply.github.com>
2023-11-06 15:25:13 +0100

    Update pagesbeshir.tsx

--- src/pages/pagesbeshir.tsx
+++ src/pages/pagesbeshir.tsx
@@ -11,5 +11,6 @@ export default function pagesbeshir() {
       <p> gnochi al pesto </p>
       <p> salmone con patate e limone </p>
       <p> tiramisu </p>
+      <p> utilizando git rebase -i HEAD~4 ho visto gli ultimi 4 commit poi sostituendo pick con squash per ogni commit ho potuto unirli insieme utilizando git rebase</p>
     );
   }
