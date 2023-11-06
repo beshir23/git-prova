c82541c57bf7eb8f1e36ab2957daeeeb46000580
beshir23 <128391475+beshir23@users.noreply.github.com>
2023-11-06 15:45:39 +0100

    Update pagebeshir.tsx

--- src/components/beshir/pagebeshir.tsx
+++ src/components/beshir/pagebeshir.tsx
@@ -4,5 +4,6 @@
       <p> yasobi </p>
       <p> jhonny deep </p>
       <p> lionel messi </p>
+      <p> es 6 punto 8 ho utilizato gi rebase per unire tutti i comit in un unico cimmit </p>
     );
   }
