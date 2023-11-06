cb009ce958e6e0c6e257251b4fafc3635ee8b81b
beshir23 <beshirkareem2@gmail.com>
2023-11-06 16:29:24 +0100

    ho il potere dell'occhio di agamotto

--- src/pages/pagesbeshir.tsx
+++ src/pages/pagesbeshir.tsx
@@ -7,5 +7,7 @@ export default function pagesbeshir() {
     return (
       <h1>kareem beshir</h1>
       <p> es 3 punto 6 ho usato git rebase ma non sono sicuro sia giusto</p>
+      <p> utilizando git reset si puo tornare a un commit precedente </p>
+      
     );
   }
