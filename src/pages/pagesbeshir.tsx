46250bba4283e3ca42e5955f035a5706d69fdb7a
beshir23 <128391475+beshir23@users.noreply.github.com>
2023-11-06 15:17:03 +0100

    Update pagesbeshir.tsx

--- src/pages/pagesbeshir.tsx
+++ src/pages/pagesbeshir.tsx
@@ -9,6 +9,7 @@ export default function pagesbeshir() {
       <p> es 3 punto 6 ho usato git rebase ma non sono sicuro sia giusto</p>
       <p> nuvole di drago </p>
       <p> gnochi al pesto </p>
+      <p> salmone con patate e limone </p>
 
     );
   }
