3873dcd9e04c0f562e8e19c22bd1c0a11cd2c973
beshir23 <beshirkareem2@gmail.com>
2023-11-06 16:32:35 +0100

    devo ancora decidere

--- src/pages/pagesbeshir.tsx
+++ src/pages/pagesbeshir.tsx
@@ -8,6 +8,6 @@ export default function pagesbeshir() {
       <h1>kareem beshir</h1>
       <p> es 3 punto 6 ho usato git rebase ma non sono sicuro sia giusto</p>
       <p> utilizando git reset si puo tornare a un commit precedente </p>
-      
+      <p> oppure ho la delorian</p>
     );
   }
